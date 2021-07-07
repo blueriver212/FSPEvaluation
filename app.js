ed094b59ce7c06b207ecef9d9f6b52475b381911
blueriver212 <53503060+blueriver212@users.noreply.github.com>
2021-07-07 17:48:25 +0100

    Fix: Removing details

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ console.log(process.env.DATABASE_URL);
 
 const client = new Client({
 	connectionString: process.env.DATABASE_URL,
-	database: "d6v2ejscl1vh5e", 
-	host: "ec2-34-254-69-72.eu-west-1.compute.amazonaws.com",
-	user: "gucinuctgseutr", 
-	password: "1b05716bb6382fc7e2ab5aa56ae1693b8fd62ec361f063a7ff2427e1be4047ac", 
+	database: , // please add your own
+	host: , // please add your own
+	user: , // please add your own
+	password: , // please add your own
 	port: 5432,
 	ssl: { 
         rejectUnauthorized: false 
