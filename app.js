bcb27931398e545d68963e5c50ca80eb14d43fa2
blueriver212 <53503060+blueriver212@users.noreply.github.com>
2021-07-07 12:09:20 +0100

    Feature: Should be the final push for the test site, for satellites

--- app.js
+++ app.js
@@ -1,81 +1,7 @@
-// // the aim is to be able to set up an API that can returna geoJSON in a file
-// // once this is complete, can i return a file by its specific name?
-
-// // install required modules
-// var express = require('express');
-// var path = require('path');
-// var app = express();
-// var fs = require('fs');
-
-// // create a http server for the files
-// var http = require('http'); var https = require('https');
-// const { ppid } = require('process');
-
-// // var httpServer = http.createServer(app);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Our app is running on port ${ PORT }`);
-// });
-
-// app.use(function(req, res, next) {
-// 	res.setHeader("Access-Control-Allow-Origin", "*");
-// 	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-// 	res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-// 	next();
-// });
-
-// app.get('/',function (req, res) {
-// 	res.send("Hello world from the data API on port"+PORT)
-// });
-
-// app.use(function(req,res,next) {
-// 	var filename = path.basename(req.url);
-// 	var extension = path.extname(filename);
-// 	console.log("The file "+filename+" was requested.");
-// 	next(); // this is what i could have been missing
-// })
-
-
-// app.get('/test', function(req, res) {
-// 	res.send({
-// 		"array": [
-// 		  1,
-// 		  2,
-// 		  3
-// 		],
-// 		"boolean": true,
-// 		"color": "gold",
-// 		"null": null,
-// 		"number": 123,
-// 		"object": {
-// 		  "a": "b",
-// 		  "c": "d"
-// 		},
-// 		"string": "Hello World"
-// 	  })
-// });
-
-
-// // app.get('/:date', function(req, res) {
-// // 	var date = req.params.date;
-// // 	var d = date.toString();
-// // 	const file = d.concat('.json');
-// // 	console.log(file);
-// // 	res.sendFile(path.join(__dirname, '/', file));
-// // })
-
-
-
-// const geoJSON = require('./routes/geoJSON');
-// app.use('/', geoJSON);
-
-// const bodyParser = require('body-parser');
-// //crud.use(bodyParser.urlencoded({extended:true}));
+// required packages for the API
 
 require('dotenv').config();
 const express = require('express');
-//const cors = require('cors');
 var pg = require('pg');
 const Client = require('pg').Client;
 pg.defaults.ssl = true;
@@ -99,8 +25,8 @@ const client = new Client({
 const app = express();
 
 app.use(express.json());
-//app.use(cors());
 
+// this route will return all of the test satellites
 app.get('/allSatellites', function (req, res) {
     client.connect(function (err, client, done) {
         if (err) {
