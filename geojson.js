e593e41cbf8ced6c9327aee5caab546266560a66
blueriver212 <indy.brownhall@gmail.com>
2021-07-09 17:29:59 +0100

    Feature: Upgraded the database to include all of the satellites, then have also updated the sql so that the year can just be queried, all of the satellites launched before hand will return

--- geojson.js
+++ geojson.js
@@ -5,10 +5,10 @@ var fs = require('fs');
 
 var config = {
     	connectionString: process.env.DATABASE_URL,
-    	database: "d6v2ejscl1vh5e", // please add your own
-    	host: "ec2-34-254-69-72.eu-west-1.compute.amazonaws.com", // please add your own
-    	user: "gucinuctgseutr", // please add your own
-    	password: "1b05716bb6382fc7e2ab5aa56ae1693b8fd62ec361f063a7ff2427e1be4047ac", // please add your own
+    	database: "ddcd35b2sfr4q9", // please add your own
+    	host: "ec2-54-155-87-214.eu-west-1.compute.amazonaws.com", // please add your own
+    	user: "cmlrcklxedxpfi", // please add your own
+    	password: "f16f4201d97ec6dba2e4410d2575c3dc4b783d8281a93b231bb1f16f1952bda0", // please add your own
     	port: 5432,
     	ssl: { 
             rejectUnauthorized: false 
@@ -31,7 +31,7 @@ geoJSON.get('/allSatellites', function (req, res) {
 
         // want to return just 10 of the rows to test to see if it works
 
-        var query = "select * from satellite_population.test limit 10;"
+        var query = "select * from satellite_population.fsp limit 10;"
         console.log(query);
 
         
@@ -65,7 +65,10 @@ geoJSON.get('/:year', function (req, res) {
         // var query = "select * from satellite_population.test";
         // query = query + " where launch_date >= '"+year+"-01-01' AND  launch_date <=  '"+year+"-12-31';";
 
-        var query = "select * from satellite_population.test where launch_date >= '"+year+"-01-01' and launch_date <= '"+year+"-12-31' limit 10;";
+        //var query = "select * from satellite_population.fsp where launch_date >= '"+year+"-01-01' and launch_date <= '"+year+"-12-31' limit 10;";
+        var query = "select * from satellite_population.fsp where launch_date <= '"+year+"-12-31';";
+
+        
         console.log(query);
         client.query(query, function (err, result) {
             done();
--- postGISConnection.js
+++ postGISConnection.js
@@ -1,5 +1,5 @@
-host: ec2-34-254-69-72.eu-west-1.compute.amazonaws.com,
-user: gucinuctgseutr, 
-database: d6v2ejscl1vh5e, 
-password: 1b05716bb6382fc7e2ab5aa56ae1693b8fd62ec361f063a7ff2427e1be4047ac, 
+host: ec2-54-155-87-214.eu-west-1.compute.amazonaws.com,
+user: cmlrcklxedxpfi, 
+database: ddcd35b2sfr4q9, 
+password: f16f4201d97ec6dba2e4410d2575c3dc4b783d8281a93b231bb1f16f1952bda0, 
 port: 5432
