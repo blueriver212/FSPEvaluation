13377da1fc5232f9322a6a1eca9ab3d5c36807d9
blueriver212 <indy.brownhall@gmail.com>
2023-01-13 16:31:49 +0000

    reorganising files

--- postGISConnection.js
+++
@@ -1,5 +0,0 @@
-host: ec2-54-155-87-214.eu-west-1.compute.amazonaws.com,
-user: cmlrcklxedxpfi, 
-database: ddcd35b2sfr4q9, 
-password: f16f4201d97ec6dba2e4410d2575c3dc4b783d8281a93b231bb1f16f1952bda0, 
-port: 5432
